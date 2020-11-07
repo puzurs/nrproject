fcd18caceaff35acea23f08e1714ec5659a0856f
Maris <petrovs.maris@gmail.com>
2020-11-07 11:08:03 +0000

    export module to vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
+module.exports = (req, res) => {
     res.send({hi: "there"})
-})
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(5000)
